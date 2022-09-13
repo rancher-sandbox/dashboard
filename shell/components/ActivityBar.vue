d5397a7670c7386230cf85b73959b31d014b363e
Phillip Rak <rak.phillip@gmail.com>
2022-09-13 14:51:20 -0700

    Update menu bar styles
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/components/ActivityBar.vue
+++ shell/components/ActivityBar.vue
@@ -1,23 +1,40 @@
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'ActivityBar',
+  data() {
+    return { isExpanded: true };
+  }
+});
+</script>
+
 <template>
   <div class="activity-bar">
     <div class="menu">
-      <svg
-        class="menu-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        height="24"
-        viewBox="0 0 24 24"
-        width="24"
-      >
-        <path d="M0 0h24v24H0z" fill="none" />
-        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-      </svg>
+      <div class="menu-container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+        </svg>
+      </div>
     </div>
     <div class="body">
-      <div class="activity-icon">
-        <span class="icon icon-2x icon-info"></span>
+      <div class="activity">
+        <div class="activity-icon">
+          <span class="icon icon-2x icon-info"></span>
+        </div>
+        <span v-if="isExpanded">Rancher Desktop</span>
       </div>
-      <div class="activity-icon active">
-        <span class="icon icon-2x icon-warning"></span>
+      <div class="activity">
+        <div class="activity-icon active">
+          <span class="icon icon-2x icon-warning"></span>
+        </div>
+        <span v-if="isExpanded">Dashboard</span>
       </div>
     </div>
   </div>
@@ -29,48 +46,65 @@
     flex-direction: column;
     align-items: stretch;
     background: #002C40 0% 0% no-repeat padding-box;
-    width: 4rem;
+    width: 16rem;
     box-shadow: 4px 3px 6px #00000029;
     opacity: 1;
     z-index: 9;
+    padding-left: 0.75rem;
 
     .menu {
       display: flex;
-      align-items: center;
-      justify-content: center;
       padding: 0.75rem 0;
-      cursor: pointer;
 
-      .menu-icon {
-        width: 24px;
-        height: 24px;
+      .menu-container {
         fill: var(--darker-text);
+        color: var(--darker-text);
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-width: 36px;
+        min-height: 36px;
+        opacity: 1;
+
+        .menu-icon {
+          color: var(--darker-text);
+          min-width: 24px;
+          min-height: 24px;
+        }
       }
+
     }
 
     .body {
       display: flex;
       gap: 1rem;
       flex-direction: column;
-      align-items: center;
-      justify-content: center;
 
-      .activity-icon {
-        &.active {
-          border: 1px solid #0181BE;
-          background-color: #0181BE;
-          color: var(--darker-text)
-        }
-
+      .activity {
         display: flex;
+        color: var(--darker-text);
+        gap: 1rem;
         align-items: center;
-        justify-content: center;
-        width: 36px;
-        height: 36px;
-        border: 1px solid #BCBCBC;
-        color: #BCBCBC;
-        border-radius: 8px;
-        opacity: 1;
+        cursor: pointer;
+
+        .activity-icon {
+          &.active {
+            border: 1px solid #0181BE;
+            background-color: #0181BE;
+            color: var(--darker-text);
+          }
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          min-width: 36px;
+          min-height: 36px;
+          border: 1px solid #BCBCBC;
+          color: #BCBCBC;
+          border-radius: 8px;
+          opacity: 1;
+        }
       }
     }
   }
